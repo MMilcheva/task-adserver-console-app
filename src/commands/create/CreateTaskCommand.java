e5f64f5e85827d44f7f4b162700110e0964bcf6a
Mariana <mariana_milcheva@abv.bg>
2023-07-21 15:47:06 +0300

    Add Javadoc

--- src/commands/create/CreateTaskCommand.java
+++ src/commands/create/CreateTaskCommand.java
@@ -13,6 +13,9 @@ import java.util.List;
 
 import static utils.ConstantHelpers.TASK_CREATED_MESSAGE;
 
+/**
+ * The class creates a Task with the given user input parameters
+ */
 public class CreateTaskCommand extends BaseCommand {
 
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 3;
@@ -24,6 +27,10 @@ public class CreateTaskCommand extends BaseCommand {
         super(taskManagementSystemRepository);
     }
 
+    /**
+     * @param parameters represent the user input data
+     * @return String message with the newly created task`s details
+     */
     @Override
     public String executeCommand(List<String> parameters) {
         ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
@@ -34,6 +41,10 @@ public class CreateTaskCommand extends BaseCommand {
         return String.format(TASK_CREATED_MESSAGE, task.getId(), task.getTitle(), task.getDescription(), dueDate, task.getStatus());
     }
 
+    /**
+     *
+     * @param parameters
+     */
     public void parseParameters(List<String> parameters) {
         try {
             this.title = parameters.get(0);
